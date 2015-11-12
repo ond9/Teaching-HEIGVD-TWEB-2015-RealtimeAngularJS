d15625ce51ff4bfd68ae73fd1ac9ae7fc4102f11
ond9 <cyrill.zundler@heig-vd.ch>
2015-11-12 16:18:23 +0100

    update config for heroku

--- config/config.js
+++ config/config.js
@@ -1,6 +1,10 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
     env = process.env.NODE_ENV || 'development';
+	 
+	 // set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
 
 var config = {
   development: {
