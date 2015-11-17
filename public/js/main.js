0eba9539997bb7a36aed950d3eb37f9d671c74c8
ond9 <cyrill.zundler@heig-vd.ch>
2015-11-17 11:29:08 +0100

    fix navbar active element

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,10 @@
 $( document ).ready(function() {
+	$('.nav li').removeClass('active');
+	
+	$('.navbar-brand').click(function(){
+		$('.nav li').removeClass('active');
+	});
+	
 	$('.nav li').click(function(){
 		 $(this).addClass('active').siblings().removeClass('active');
 	});
