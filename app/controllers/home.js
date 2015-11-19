7d6e6790ca574837c1c412080027a1663681208f
ond9 <cyrill.zundler@heig-vd.ch>
2015-11-19 11:54:53 +0100

    fix nav refresh with html 5 mode

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -7,13 +7,21 @@ module.exports = function (app) {
   app.use('/', router);
 };
 
+
+
 router.get('/template/:name.html', function(req, res) {
 	console.log(req.params.name);
 	res.render('template/' + req.params.name);
 })
 
+
+
 router.get('/', function (req, res, next) {
     res.render('index', {
       title: 'MyFirstAngularJSRealtimeCharts',
     });
+});
+
+router.get('*', function(req, res){
+    res.render('index');
 });
